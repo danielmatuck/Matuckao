27756568e8923a72401115faee4a9ec04cf71bdf
daniel.mmatuck <danielmatuck@hotmail.com>
2018-09-20 20:28:36 -0300

    Distancia entre os ponto

---
+++ Vai Palmeiras 02/main 02.py
@@ -0,0 +1,7 @@
+import math
+x1= int(input("Coordenada x1 de P1: "))
+y1= int(input("Coordenada de y1 de P1: "))
+x2= int(input("Coordenada x2 de P2: "))
+y2= int(input("Coordenada y2 de P2: "))
+d=math.sqrt((x1-x2)**2 + (y1-y2)**2)
+print ("A distancia dos pontos eh",d )
