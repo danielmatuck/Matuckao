f9604cd7f1b6414e3716e1104e09d4b425ea35ea
daniel.mmatuck <danielmatuck@hotmail.com>
2018-09-20 20:11:09 -0300

    versão inicial do meu projeto

--- Vai Palmeiras/main.py
+++ Vai Palmeiras/main.py
@@ -1,2 +1,14 @@
-print("oi, mundo")
-print("Gui gente boa")
+import math
+
+a = int(input("Informe o valor de a: "))
+b = int(input("Informe o valor de b: "))
+c = int(input("Informe o valor de c: "))
+delta = b * b - 4 * a * c
+print("Delta: ",delta)
+if delta >= 0:
+    x1=(-b + math.sqrt(delta)) / (2*a)
+    x2=(-b - math.sqrt(delta)) / (2*a)
+    print("Raiz x1:",x1)
+    print("Raiz x:",x2)
+else:
+    print("Não existem raízes reais")
